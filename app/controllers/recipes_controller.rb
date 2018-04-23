3c4d13722e299cb74261c8aa17f66ced31eae6c0
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-23 15:22:18 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,5 +14,17 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :name,
+      :quantity
+      ] )
   end
 end
